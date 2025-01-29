7313c110af51d4e1ffbe0ba1661e2147108de774
hazepynut <naden.hey@gmail.com>
2025-01-29 15:45:02 +0800

    gold

--- anykernel.sh
+++ anykernel.sh
@@ -4,14 +4,14 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=QuartiX GKI DUMMY1 (DATE) KSUDUMMY2
-do.devicecheck=0
+kernel.string=Kernel DUMMY1 (DATE) KSUDUMMY2
+do.devicecheck=1
 do.modules=0
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=
-device.name2=
+device.name1=gold
+device.name2=iron
 device.name3=
 device.name4=
 device.name5=
@@ -30,20 +30,6 @@ PATCH_VBMETA_FLAG=auto;
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh
 
-kernel_version=$(cat /proc/version | awk -F '-' '{print $1}' | awk '{print $3}')
-case "$kernel_version" in
-    5.10.*) supp=true ;;
-    *) supp=false ;;
-esac
-
-ui_print " " "-> 5.10 Kernel: $supp"
-$supp || exit 1
-
 # boot install
-if [ -L "/dev/block/bootdevice/by-name/init_boot_a" -o -L "/dev/block/by-name/init_boot_a" ]; then
-    split_boot # for devices with init_boot ramdisk
-    flash_boot # for devices with init_boot ramdisk
-else
-    dump_boot # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-    write_boot # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
-fi
+split_boot # for devices with init_boot ramdisk
+flash_boot # for devices with init_boot ramdisk
